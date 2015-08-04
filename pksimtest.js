caadc667515c6fce7ed7d3b2883b1566038a3a8a
Devin Pastoor <devin.pastoor@gmail.com>
2015-08-04 13:37:31 -0400

    continuing to test 1cmptiv

--- pksimtest.js
+++ pksimtest.js
@@ -1,5 +1,5 @@
 var pksim = require('./lib/index.js');
-
+var _ = require('lodash');
 var seq = [];
 for (var i = 0; i <= 48; i++) {
 	seq.push(i);
@@ -7,4 +7,22 @@ for (var i = 0; i <= 48; i++) {
 
 var reg = {doses: [100, 10, 1], times: [0, 12, 24]};
 
-console.log(pksim.sampleIntervals(reg, seq));
+var regimen = pksim.sampleIntervals(reg, seq);
+console.log(regimen);
+
+
+var sim = [];
+
+var lastAmt = 0;
+var Cl = 1;
+var Vd = 10;
+var simInterval;
+for(var i = 0; i < regimen.length; ++i) {
+	console.log(lastAmt);
+	simInterval = pksim.onecmptiv(Cl, Vd, regimen[i].dose + lastAmt, regimen[i].time);
+	lastAmt = simInterval[simInterval.length-1].y*Vd;
+	sim.push(simInterval);
+	console.log(simInterval);
+}
+
+console.log(sim);
