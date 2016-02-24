932130ab640105313654edebf6a981c43b44a66a
Devin Pastoor <devin.pastoor@gmail.com>
2016-02-23 22:34:41 -0500

    reference dose values

--- pksimtest.js
+++ pksimtest.js
@@ -1,10 +1,10 @@
 import {oneCmptIvBolus, sampleIntervals, oldSampleIntervalFunction} from './src/index.js';
 import _ from 'lodash';
-
+import fs from 'fs';
 console.log(oneCmptIvBolus(1,10, 100, [0, 1, 2, 3, 4, 5]));
 console.time('sampleIntervals');
 let intervals = sampleIntervals({
-	doses: [100, 10, 1],
+	doses: [200, 50, 50],
 	times: [0, 12, 24]
 }, _.range(0, 36, 0.5));
 console.timeEnd('sampleIntervals');
@@ -13,13 +13,15 @@ console.time('onecmpt');
 
 let onecmpt = _.reduce(intervals, (acc, value, i, array) => {
 	if (!acc.length) {
-		return acc.concat(oneCmptIvBolus(1,10, 100, value.times));
+		return acc.concat(oneCmptIvBolus(1,10, value.dose, value.times));
 	}
 	let lastConc = acc[acc.length - 1].dv;
-	return acc.concat(oneCmptIvBolus(1,10, 100, value.times, lastConc));
+	return acc.concat(oneCmptIvBolus(1,10, value.dose, value.times, lastConc));
 }, []);
 console.timeEnd('onecmpt');
 console.log(onecmpt)
+
+fs.writeFileSync('pkexample.json', JSON.stringify(onecmpt,null, 4));
 //
 // var seq = [];
 // for (var i = 0; i <= 48; i++) {
