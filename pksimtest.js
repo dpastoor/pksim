279a142fb6609e03fc940c66de559143dd588c92
Devin Pastoor <devin.pastoor@gmail.com>
2016-02-24 14:27:34 -0500

    tests with one cmpt oral as well

--- pksimtest.js
+++ pksimtest.js
@@ -1,17 +1,17 @@
-import {oneCmptIvBolus, sampleIntervals, oldSampleIntervalFunction} from './src/index.js';
+import {oneCmptIvBolus, sampleIntervals, oneCmptOral} from './src/index.js';
 import _ from 'lodash';
 import fs from 'fs';
 console.log(oneCmptIvBolus(1,10, 100, [0, 1, 2, 3, 4, 5]));
 console.time('sampleIntervals');
 let intervals = sampleIntervals({
-	doses: [200, 50, 50],
+	doses: [100, 100, 100],
 	times: [0, 12, 24]
 }, _.range(0, 36, 0.5));
+
 console.timeEnd('sampleIntervals');
 // console.log(intervals);
 console.time('onecmpt');
-
-let onecmpt = _.reduce(intervals, (acc, value, i, array) => {
+let onecmptiv = _.reduce(intervals, (acc, value, i, array) => {
 	if (!acc.length) {
 		return acc.concat(oneCmptIvBolus(1,10, value.dose, value.times));
 	}
@@ -19,9 +19,34 @@ let onecmpt = _.reduce(intervals, (acc, value, i, array) => {
 	return acc.concat(oneCmptIvBolus(1,10, value.dose, value.times, lastConc));
 }, []);
 console.timeEnd('onecmpt');
-console.log(onecmpt)
+//
+// let onecmptoraldosetimes = _.reduce(intervals, (acc, value, i, array) => {
+// 	if (!acc.hasOwnProperty("dose")) {
+// 		acc.dose = value.dose;
+// 		acc.times = value.times;
+// 		acc.numDoses = 1;
+// 		acc.tau = array[1].times[0] - array[0].times[0];
+// 	} else {
+// 		acc.times = acc.times.concat(value.times);
+// 		acc.numDoses = acc.numDoses + 1;
+// 	}
+// 	return acc
+// }, {});
+//
+// let onecmptoral = oneCmptOral(1, 10, 1, onecmptoraldosetimes.tau, onecmptoraldosetimes.dose, 1, onecmptoraldosetimes.times)
+console.time('onecmptoral');
+let onecmptoral = _.reduce(intervals, (acc, value, i, array) => {
+	if (!acc.length) {
+		return acc.concat(oneCmptOral(1,10, 1, 1, value.dose, value.doseNum, value.times))
+	}
+	let lastConc = acc[acc.length - 1].dv;
+	return acc.concat(oneCmptOral(1,10, 1, 12, value.dose, value.doseNum, value.times, lastConc));
+}, []);
+ // console.log(onecmptoral);
+console.timeEnd('onecmptoral');
 
-fs.writeFileSync('pkexample.json', JSON.stringify(onecmpt,null, 4));
+// fs.writeFileSync('onecmptiv.json', JSON.stringify(onecmptiv,null, 4));
+ fs.writeFileSync('onecmptoral.json', JSON.stringify(onecmptoral,null, 4));
 //
 // var seq = [];
 // for (var i = 0; i <= 48; i++) {
